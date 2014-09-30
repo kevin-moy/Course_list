43f9d2b72e9c49b0a097c14878daaf5dbd15e941
Kevin Moy <kevintmoy@gmail.com>
2014-09-30 11:31:48 -0700

    webview and credentials

---
+++ Nerdfeed/CoursesViewController.h
@@ -0,0 +1,17 @@
+//
+//  CoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Kevin Moy on 9/17/14.
+//  Copyright (c) 2014 Kevin Moy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class WebViewController;
+
+@interface CoursesViewController : UITableViewController
+
+@property (nonatomic) WebViewController *webViewController;
+
+@end
---
+++ Nerdfeed/WebViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebViewController.h
+//  Nerdfeed
+//
+//  Created by Kevin Moy on 9/30/14.
+//  Copyright (c) 2014 Kevin Moy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@property (nonatomic) NSURL *URL;
+
+@end
